ca74abc2200d7ab48e8b42a83f0b9b0fae4ca422
Pasindu Peiris <it21831454@my.sliit.lk>
2025-01-19 21:41:18 +0530

    Update index.mjs

--- server/index.mjs
+++ server/index.mjs
@@ -1,4 +1,5 @@
 import express from 'express';
+import rootRouter from './src/routes/rootRouter.mjs';
 
 const app = express();
 app.use(express.json());
@@ -6,6 +7,9 @@ app.use(express.json());
 app.listen(4005, () => {
     console.log('Server is running on port 4005');
 });
+
+//routes connections
+app.use('/api/v1', rootRouter);
 
 app.get('/', async (req, res) => {
     res.json({ message: 'Backend is running' });
