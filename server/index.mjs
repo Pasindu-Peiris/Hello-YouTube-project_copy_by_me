9a130e7fa22f62bddc69d4057561b97e79161dfe
Pasindu Peiris <it21831454@my.sliit.lk>
2025-02-04 13:46:56 +0530

    Update index.mjs

--- server/index.mjs
+++ server/index.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import rootRouter from './src/routes/rootRouter.mjs';
 import cors from 'cors';
 import 'dotenv/config';
-import DB from './src/db/db.mjs'; // Import DB for connection check
+
 
 const app = express();
 app.use(express.json());
@@ -11,18 +11,6 @@ app.use(cors());
 
 const PORT = process.env.PORT || 4005;
 
-//Check DB connection
-const checkDBConnection = async () => {
-  try {
-    await DB.$connect();
-    console.log("Database connected successfully!");
-  } catch (error) {
-    console.error("Database connection failed:", error);
-  }
- };
-
-//Call the checkDBConnection function
-checkDBConnection();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
