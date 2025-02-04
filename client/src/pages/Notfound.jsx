e24b3b7bd82d097295114ed0158af53560b10ee4
Pasindu Peiris <it21831454@my.sliit.lk>
2025-02-04 21:07:48 +0530

    Update Home and not Found page with SEO

--- client/src/pages/Notfound.jsx
+++ client/src/pages/Notfound.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import NotFound from "../assets/images/notfound.svg";
-import '../assets/pagecss/Notfound.css'
+import '../assets/pagecss/Notfound.css';
 
 const Notfound = () => {
   return (
     <div className="notfound-container">
-
+      {/* SEO Optimization */}
+      <Helmet>
+        <title>404 - Page Not Found</title>
+        <meta name="description" content="Oops! The page you're looking for doesn't exist. Return to the login page." />
+        <meta name="robots" content="noindex, nofollow" />
+      </Helmet>
+      
       <img src={NotFound} alt="Page not found" />
       <p>Oops! The page you're looking for doesn't exist.</p>
       <Link to="/">Go Back to Login</Link>
-      
     </div>
   );
 };
