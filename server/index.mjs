82663b13261cc8738b36ffae2cdcaae04c8d625a
Pasindu Peiris <it21831454@my.sliit.lk>
2025-02-04 12:58:21 +0530

    Revert "Merge branch 'dev-branch' of https://github.com/eSupportTechnology/Hello-YouTube-project into dev-branch"
    This reverts commit 22fa0ba36adc28bfa17c76ef1728eeda53dfdcd9, reversing
    changes made to 32b8c188c6d74e8875c35f1895ed18ea7edb14c6.

--- server/index.mjs
+++ server/index.mjs
@@ -10,6 +10,19 @@ app.use(cors());
 
 const PORT = process.env.PORT || 4005;
 
+//Check DB connection
+const checkDBConnection = async () => {
+  try {
+    await DB.$connect();
+    console.log("Database connected successfully!");
+  } catch (error) {
+    console.error("Database connection failed:", error);
+  }
+ };
+
+//Call the checkDBConnection function
+checkDBConnection();
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
