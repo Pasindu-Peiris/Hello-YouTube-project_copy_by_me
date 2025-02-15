07c39faa8cf63776499a9f5c5495f1ef142e6757
Pasindu Peiris <it21831454@my.sliit.lk>
2025-02-15 15:39:50 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,41 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ProtectedRouters from './utils/ProtectedRouters';
-import Notfound from './pages/Notfound';
-import Home from './pages/Home';
-import Loginuser from './pages/user/Loginuser';
-import Registration from './pages/user/Registration';
-import Userdashboard from './pages/user/Userdashboard';
-import Subadd from './pages/user/Subadd';
-import Subtask from './pages/user/Subtask';
-import Tasksubtab from './pages/user/Tasksubtab';
-import Tasksubupload from './pages/user/Tasksubupload';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectedRouters from "./utils/ProtectedRouters";
+import Notfound from "./pages/Notfound";
+import Home from "./pages/Home";
+import Loginuser from "./pages/user/Loginuser";
+import Registration from "./pages/user/Registration";
+import Subadd from "./pages/user/Subadd";
+import Subtask from "./pages/user/Subtask";
+import Tasksubtab from "./pages/user/Tasksubtab";
+import Tasksubupload from "./pages/user/Tasksubupload";
 
 function App() {
   return (
+
+
     <BrowserRouter>
 
       <Routes>
 
-        <Route path='/' element={<Home />} />
-        <Route path='/signin' element={<Loginuser />} />
-        <Route path='/signup' element={<Registration />} />
-        <Route path='/sub' element={<Subtask/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Loginuser />} />
+        <Route path="/signup" element={<Registration />} />
 
-        <Route path='/tasksub' element={<Tasksubtab />} />
-        <Route path='/tasksubcomplete/:taskid' element={<Tasksubupload/>}/>
-
-
-        
-        
         {/* protected routers */}
         <Route element={<ProtectedRouters />}>
-
-          <Route path='/user-dashboard' element={<Userdashboard />} />
-          <Route path='/dash' element={<Subadd />} />
-
+          <Route path="/user-dashboard" element={<Subadd />} />
+          <Route path="/subscription" element={<Subtask />} />
+          <Route path="/tasksubscription" element={<Tasksubtab />} />
+          <Route path="/tasksubcomplete/:taskid" element={<Tasksubupload />} />
         </Route>
 
         {/* 404 Page Route */}
-        <Route path='*' element={<Notfound />} />
-
-
+        <Route path="*" element={<Notfound />} />
+        
       </Routes>
 
     </BrowserRouter>
-
-
   );
 }
 
-export default App; 
+export default App;
