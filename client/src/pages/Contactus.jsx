9c96b032c30af32d8594944134cd4960d310418e
Pasindu Peiris <it21831454@my.sliit.lk>
2025-03-05 15:46:43 +0530

    Update Contactus.jsx

--- client/src/pages/Contactus.jsx
+++ client/src/pages/Contactus.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../assets/pagecss/Contactus.css'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 const Contactus = () => {
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("Form Submitted");
+
+        console.log(name, email, message)
+    }
+
+
+
     return (
         <div>
 
@@ -12,19 +28,40 @@ const Contactus = () => {
             <div className="contactus">
 
                 <div className='contactustaxt'>
+                    
+
+                    <div className="contact-form-new">
                     <h1>Contact us</h1>
+                        <form onSubmit={handleSubmit}>
+                            <input id="name" name="name" value={name} onChange={(e) => {
+                               setName( e.target.value)
+                            }} placeholder="Enter Your Name" required />
+                            <input id="email" name="email" value={email} onChange={(e) => {
+                               setEmail( e.target.value)
+                            }} placeholder="Enter Your Email" required />
+                            <textarea placeholder="Enter Your Message" onChange={(e) => {
+                               setMessage( e.target.value)
+                            }} value={message}></textarea>
+                            <input id="submit" name="submit" type="submit" value="Send" />
 
-                    <p>We are happy to answer any questions you may have about Hello YouTube. For faster communication, we recommend
-                        to contact us via email.</p>
+                        </form>
+                    </div>
 
-                    <div class="links-contact">
-                        <ul>
-                           
-                            <li><i class="fa-solid fa-phone"></i> +94 099 3443</li>
-                            <li><i class="fa-solid fa-envelope"></i> helloyt@gmail.com</li>
-                        </ul>
+                    <div className="text-form-new">
+                        <p>We are happy to answer any questions you may have about Hello YouTube. <br /> For faster communication, we recommend
+                            to contact us via email.</p>
+
+                        <div class="links-contact">
+                            <ul>
+
+                                <li><i class="fa-solid fa-phone"></i> +94 099 3443</li>
+                                <li><i class="fa-solid fa-envelope"></i> helloyt@gmail.com</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
+
+
 
 
 
